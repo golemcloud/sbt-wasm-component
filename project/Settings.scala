a9f7c5ae6db3cafa9d72bf824cda7186d8b8bfcf
Daniele Torelli <daniele.torelli@ziverge.com>
2024-05-08 16:00:27 -0600

    Add publishing settings (#11)
    * Add publishing settings
    * Add last LTS
    * Reconfigure triggers

--- project/Settings.scala
+++ project/Settings.scala
@@ -5,6 +5,39 @@ object Settings {
 
   implicit final class ProjectSettings(project: sbt.Project) {
 
+    def publishSettings: sbt.Project = {
+      import com.jsuereth.sbtpgp.PgpKeys.*
+
+      project.settings(
+        inThisBuild(
+          List(
+            organization := "cloud.golem",
+            homepage := Some(
+              url("https://github.com/golemcloud/sbt-wasm-component")
+            ),
+            licenses := List(
+              "Apache-2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0")
+            ),
+            developers := List(
+              Developer(
+                "danieletorelli",
+                "Daniele Torelli",
+                "daniele.torelli@ziverge.com",
+                url("https://mdtorelli.it")
+              )
+            ),
+            resolvers +=
+              "Sonatype OSS Snapshots 01" at "https://s01.oss.sonatype.org/content/repositories/snapshots",
+            pgpPassphrase := sys.env.get("PGP_PASSWORD").map(_.toArray),
+            pgpPublicRing := file("/tmp/public.asc"),
+            pgpSecretRing := file("/tmp/secret.asc"),
+            resolvers +=
+              "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
+          )
+        )
+      )
+    }
+
     def macroParadiseSettings: sbt.Project =
       project.settings(
         scalacOptions ++= {
