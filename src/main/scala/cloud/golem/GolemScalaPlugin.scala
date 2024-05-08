29a798da837b5d3541cf288e9ba7efc98aa6d9f5
Daniele Torelli <daniele.torelli@ziverge.com>
2024-05-08 14:33:47 -0600

    Add settings and tasks (#3)
    * Add settings and tasks
    * Fix tests
    * Cleanup settings
    * Add golemScalaOutputDirectory

--- src/main/scala/cloud/golem/GolemScalaPlugin.scala
+++ src/main/scala/cloud/golem/GolemScalaPlugin.scala
@@ -1,39 +1,40 @@
 package cloud.golem
 
 import sbt.*
-import sbt.Keys.*
 import org.scalajs.sbtplugin.ScalaJSPlugin
-import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport.*
 import sbt.plugins.JvmPlugin
 
 object GolemScalaPlugin extends AutoPlugin {
-  private object Versions {
-    val macros = "0.1.0"
-    val scalaMacrosParadise = "2.1.1"
+  object autoImport {
+    lazy val golemScalaOutputDirectory = SettingKey[File](
+      "golemScalaOutputDirectory",
+      "Output directory",
+      KeyRanks.Invisible
+    )
+    lazy val golemScalaWitPath = SettingKey[File](
+      "golemScalaWitPath",
+      "Path to the wit file",
+      KeyRanks.Invisible
+    )
+    lazy val golemScalaPackageName = SettingKey[String](
+      "golemScalaPackageName",
+      "Package name",
+      KeyRanks.Invisible
+    )
+    lazy val witBindgen =
+      taskKey[Seq[File]](
+        "Runs golem-scalajs-wit-bindgen to generate WIT bindings"
+      )
+    lazy val component =
+      taskKey[Unit]("Runs componentize-js on the generated main.js file")
   }
 
   override def trigger: PluginTrigger = allRequirements
 
   override def requires: Plugins = JvmPlugin && ScalaJSPlugin
 
-  override lazy val projectSettings: Seq[Setting[?]] = Seq(
-    scalaJSLinkerConfig ~= { _.withModuleKind(ModuleKind.ESModule) },
-    libraryDependencies += "cloud.golem" %% "golem-scala-macros" % Versions.macros
-  ) ++ macroParadiseSettings
-
-  private lazy val macroParadiseSettings = Seq(
-    scalacOptions ++= {
-      if (scalaVersion.value.startsWith("2.13")) Seq("-Ymacro-annotations")
-      else Nil
-    },
-    libraryDependencies ++= {
-      if (scalaVersion.value.startsWith("2.12")) {
-        Seq(
-          compilerPlugin(
-            "org.scalamacros" % "paradise" % Versions.scalaMacrosParadise cross CrossVersion.full
-          )
-        )
-      } else Nil
-    }
-  )
+  override lazy val projectSettings: Seq[Setting[?]] =
+    GolemScalaPluginInternal.baseSettings ++
+      GolemScalaPluginInternal.scalaJsSettings ++
+      GolemScalaPluginInternal.macroParadiseSettings
 }
