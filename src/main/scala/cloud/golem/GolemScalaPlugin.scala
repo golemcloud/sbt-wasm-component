ba57fc7a1f62d38be8bd2e10a89f0c200064521b
Daniele Torelli <daniele.torelli@ziverge.com>
2024-05-08 11:31:26 -0600

    Add settings and tasks

--- src/main/scala/cloud/golem/GolemScalaPlugin.scala
+++ src/main/scala/cloud/golem/GolemScalaPlugin.scala
@@ -4,36 +4,57 @@ import sbt.*
 import sbt.Keys.*
 import org.scalajs.sbtplugin.ScalaJSPlugin
 import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport.*
+import sbt.KeyRanks.BSetting
 import sbt.plugins.JvmPlugin
 
 object GolemScalaPlugin extends AutoPlugin {
   private object Versions {
     val macros = "0.1.0"
-    val scalaMacrosParadise = "2.1.1"
   }
+
+  object autoImport {
+    val outputDirectory =
+      SettingKey[File](
+        "outputDirectory",
+        "Directory for linker output",
+        BSetting
+      )
+
+    val witBindgen =
+      taskKey[Unit]("Runs golem-scalajs-wit-bindgen to generate WIT bindings")
+
+    val component =
+      taskKey[Unit]("Runs componentize-js on the generated main.js file")
+
+    lazy val baseSettings: Seq[Setting[?]] =
+      Def.settings(
+        scalacOptions := Seq("-Ymacro-annotations"),
+        witBindgen := {
+          import scala.sys.process.*
+          Seq(
+            "bash",
+            "-xc",
+            "golem-scalajs-wit-bindgen -w wit/main.wit -p example"
+          ).!!
+        },
+        component := {
+          import scala.sys.process.*
+          Seq("bash", "-xc", "npm install").!!
+          Seq("bash", "-xc", "npm run build").!!
+        },
+        component := (component dependsOn (Compile / fullLinkJS)).value
+      )
+  }
+
+  import autoImport.*
 
   override def trigger: PluginTrigger = allRequirements
 
   override def requires: Plugins = JvmPlugin && ScalaJSPlugin
 
-  override lazy val projectSettings: Seq[Setting[?]] = Seq(
+  override lazy val projectSettings: Seq[Setting[?]] = baseSettings ++ Seq(
     scalaJSLinkerConfig ~= { _.withModuleKind(ModuleKind.ESModule) },
+    Compile / fullLinkJS / scalaJSLinkerOutputDirectory := outputDirectory.value,
     libraryDependencies += "cloud.golem" %% "golem-scala-macros" % Versions.macros
-  ) ++ macroParadiseSettings
-
-  private lazy val macroParadiseSettings = Seq(
-    scalacOptions ++= {
-      if (scalaVersion.value.startsWith("2.13")) Seq("-Ymacro-annotations")
-      else Nil
-    },
-    libraryDependencies ++= {
-      if (scalaVersion.value.startsWith("2.12")) {
-        Seq(
-          compilerPlugin(
-            "org.scalamacros" % "paradise" % Versions.scalaMacrosParadise cross CrossVersion.full
-          )
-        )
-      } else Nil
-    }
   )
 }
